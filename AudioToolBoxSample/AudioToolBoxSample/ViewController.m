7c3a46ea4e09c78ccd2fc17ec6beeea7e285a675
Daiki Asahi <dalto.saxphone@gmail.com>
2014-07-30 15:24:40 +0900

    Implement roop.

--- AudioToolBoxSample/AudioToolBoxSample/ViewController.m
+++ AudioToolBoxSample/AudioToolBoxSample/ViewController.m
@@ -27,13 +27,26 @@
 @synthesize stopButton;
 @synthesize fanfareSoundId;
 
+void completionBlock(SystemSoundID soundId, void *clientData) {
+    
+    ViewController *viewController = (__bridge ViewController *)clientData;
+    if(soundId == viewController.fanfareSoundId) {
+        AudioServicesPlaySystemSound(soundId);
+        return;
+    }
+    
+    return;
+    
+};
+
 - (void)viewDidLoad {
 
     [super viewDidLoad];
 
     // fanfare.wav file is downloaded by http://www.ne.jp/asahi/music/myuu/wave/wave.htm
-    AudioServicesCreateSystemSoundID((__bridge CFURLRef)[[NSBundle mainBundle] URLForResource:@"fanfare" withExtension:@"wav"], &fanfareSoundId); // 2
-    AudioServicesPlaySystemSound(fanfareSoundId); // 3
+    AudioServicesCreateSystemSoundID((__bridge CFURLRef)[[NSBundle mainBundle] URLForResource:@"fanfare" withExtension:@"wav"], &fanfareSoundId);
+    AudioServicesAddSystemSoundCompletion(fanfareSoundId, NULL, NULL, completionBlock, (__bridge void *)self);
+    AudioServicesPlaySystemSound(fanfareSoundId);
 
 }
 
